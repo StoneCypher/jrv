57bf2cd33d3989925b1e26b5004fd38fa1908309
John Haugeland <stonecypher@gmail.com>
2016-07-25 01:07:56 -0700

    removing accidental commit of debugging console logs

--- src/js/jrv.js
+++ src/js/jrv.js
@@ -2,7 +2,6 @@
 class JRV {
 
     constructor(comp = (() => true), onchange) {
-        console.log('construction');
         this.dirty          = true;
         this.comp           = comp;
         this.change_handler = onchange;
@@ -11,14 +10,12 @@ class JRV {
     }
 
     dirty_descendants() {
-        console.log('dirtying descendants');
         this.callbacks.map(cb => cb());
     }
 
     flag_dirty() {
-        console.log('flagging dirty');
         this.dirty = true;
-        if (this.change_handler) { console.log('forcing update: ch'); this.update(); } else { console.log('no force'); }
+        if (this.change_handler) { this.update(); }
         this.dirty_descendants();
         return this;
     }
@@ -34,7 +31,7 @@ class JRV {
     }
 
     make_notifier() {
-        return (() => { console.log('notifier fired'); return this.flag_dirty(); });
+        return (() => this.flag_dirty());
     }
 
     onchange(newOnChange) {
@@ -54,24 +51,18 @@ class JRV {
 
     update() {
 
-        console.log('updating');
-
         var compIsFunc = (typeof this.comp  === 'function'),
             valIsObj   = (typeof this.value === 'object'),
             oldVal     = valIsObj? NaN : this.value;  // because NaN === NaN is false, so nothing .comp() returns will match NaN // because Javascript is gross
 
         this.dirty = false;
 
-        this.value = (compIsFunc? (console.log('  comp'), this.comp()) : (console.log('  store'), this.comp));
+        this.value = (compIsFunc? this.comp() : this.comp);
 
         if (this.value !== oldVal) {
-            console.log('  calling change handler');
             this.change_handler? this.change_handler(this.value, oldVal) : true;
-        } else {
-            console.log('  not calling change handler');
         }
 
-        console.log('  update is dirtying descendants');
         this.dirty_descendants();
 
         return this.value;
